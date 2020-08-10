cbeb84aaf172c412ce76ca3caf84b9a593b5d60e
AnthonyW <anthonylavel@gmail.com>
2020-08-10 14:15:03 -0500

    First Commit

---
+++ src/main/java/com/jasmine/stba/SmarttravelbugApplication.java
@@ -0,0 +1,13 @@
+package com.jasmine.stba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarttravelbugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarttravelbugApplication.class, args);
+	}
+
+}
